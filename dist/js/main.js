646f4cee4558fa089514dfa66186a7cdde6c9932
queeny <amarqueenykenn@gmail.com>
2021-04-14 07:38:54 +0800

    created find your candidate section, created buttons and updated navbar

--- dist/js/main.js
+++ dist/js/main.js
@@ -8,15 +8,41 @@ $(document).ready(function () {
 	// Navbar
 	// ==========================================================================
 	(function() {
-		const navTrigger = document.querySelector('.nav__icon')
-		const navLinks = document.querySelector('.nav__links')
+		const navigation = document.querySelector('.nav');
+		const navTrigger = document.querySelector('.nav__icon');
+		const navLinks = document.querySelector('.nav__links');
+
+		window.addEventListener('scroll', () => {
+			const scrollPosition = window.scrollY;
+			if (scrollPosition > 10) {
+				navigation.classList.add('is-scrolled');
+			}
+			else {
+				navigation.classList.remove('is-scrolled');
+			}
+		});
 
 		if(navTrigger) {
 			navTrigger.addEventListener('click', (e) => {
 				e.preventDefault()
 				navTrigger.classList.toggle('is-active')
 				navLinks.classList.toggle('is-active')
-				activeChecker(navLinks)
+			});
+		}
+	})();
+	// ==========================================================================
+
+	// ==========================================================================
+	// Long search dropdown
+	// ==========================================================================
+	(function() {
+		const categoryTrigger = document.querySelector('.long-search__multiselect');
+		const categoryDp = document.querySelector('.long-search__categories')
+		
+		if(categoryTrigger) {
+			categoryTrigger.addEventListener('click', (e) => {
+				e.preventDefault()
+				categoryDp.classList.toggle('is-active')
 			});
 		}
 	})();
